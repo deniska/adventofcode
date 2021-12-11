5d8fd04fe628310471b5e4dcc06d6f4142251686
Denis Pobedrya <denis.pobedrya@gmail.com>
2021-12-11 22:40:54 +0300

    2020d21p2

--- advent2020/a21.py
+++ advent2020/a21.py
@@ -1,13 +1,16 @@
 import sys
-import collections
+from collections import defaultdict
+from itertools import permutations
 
 lines = []
+all_allergens = set()
 
 with open(sys.argv[1]) as f:
     for line in f:
         a1, a2 = line.strip().split(' (contains ')
         ingredients = set(a1.split())
         allergens = set(s.strip(',)') for s in a2.split())
+        all_allergens |= allergens
         lines.append((ingredients, allergens))
 
 have_allergens = set()
@@ -26,3 +29,22 @@ for i1, a1 in lines:
         if i not in have_allergens:
             c += 1
 print(c)
+
+for i1, a1 in lines:
+    i1 &= have_allergens
+
+allergens = list(all_allergens)
+for ingredients in permutations(have_allergens):
+    valid = True
+    for ii, aa in lines:
+        for i, a in zip(ingredients, allergens):
+            if a in aa and i not in ii:
+                valid = False
+                break
+        if not valid:
+            break
+    if valid:
+        break
+
+translations = {i: a for i, a in zip(ingredients, allergens)}
+print(','.join(sorted(translations, key=lambda x: translations[x])))
