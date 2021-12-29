83aa74fc15d0a168bae17df6dde37c41517b228e
Denis Pobedrya <denis.pobedrya@gmail.com>
2021-12-29 13:20:45 +0300

    2021d22p2 twice as fast, still a bit too slow for the real input (approx 4500 minutes)

--- advent2021/a22.py
+++ advent2021/a22.py
@@ -10,11 +10,12 @@ with open(sys.argv[1]) as f:
         coords = [int(a) for a in re.findall(r'-?\d+', line)]
         instructions.append((state == 'on', coords))
 
-def part1():
-    c = 0
-    for x, y, z in itertools.product(*[range(-50, 51)]*3):
-        c += calc_point(x, y, z)
-    return c
+code = ['def calc_point1(x, y, z):']
+for s, (x1, x2, y1, y2, z1, z2) in reversed(instructions):
+    code.append(f' if {x1} <= x <= {x2} and {y1} <= y <= {y2} and {z1} <= z <= {z2}:')
+    code.append(f'  return {s}')
+code.append(' return False')
+exec('\n'.join(code))
 
 def calc_point(x, y, z):
     state = False
@@ -23,9 +24,19 @@ def calc_point(x, y, z):
             state = s
     return state
 
+def part1(calc_point=calc_point1):
+    c = 0
+    for x, y, z in itertools.product(*[range(-50, 51)]*3):
+        c += calc_point(x, y, z)
+    return c
+
 print(part1())
 
-def part2():
+def pairwise(a):
+    for i in range(len(a) - 1):
+        yield a[i], a[i+1]
+
+def part2(calc_point=calc_point1, pairwise=pairwise):
     x_splits = set()
     y_splits = set()
     z_splits = set()
@@ -40,12 +51,14 @@ def part2():
     y_splits = sorted(y_splits)
     z_splits = sorted(z_splits)
     vol = 0
-    for sx1, sx2 in itertools.pairwise(x_splits):
-        for sy1, sy2 in itertools.pairwise(y_splits):
-            for sz1, sz2 in itertools.pairwise(z_splits):
-                state = calc_point(sx1, sy1, sz1)
-                if state:
+    total = (len(x_splits) - 1) * (len(y_splits) - 1) * (len(z_splits) - 1)
+    processed = 0
+    for sx1, sx2 in pairwise(x_splits):
+        for sy1, sy2 in pairwise(y_splits):
+            for sz1, sz2 in pairwise(z_splits):
+                if calc_point(sx1, sy1, sz1):
                     vol += (sx2 - sx1) * (sy2 - sy1) * (sz2 - sz1)
+
     return vol
 
 print(part2())
