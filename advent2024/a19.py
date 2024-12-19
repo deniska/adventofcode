23f23f6b94b59e461ab0003e6160a2a7558ea8a8
Denis Pobedrya <denis.pobedrya@gmail.com>
2024-12-19 14:48:19 +0400

    2024d19 (condensed)

--- advent2024/a19.py
+++ advent2024/a19.py
@@ -1,4 +1,3 @@
-import re
 import sys
 import functools
 
@@ -8,28 +7,6 @@ with open(sys.argv[1]) as f:
     f.readline()
     for line in f:
         designs.append(line.strip())
-
-towels.sort(key=lambda x: (len(x), x))
-
-def towels_regexp(towels):
-    regexp = '({})+'.format('|'.join(towels))
-    return regexp
-
-optimized_towels = [towels[0]]
-for t in towels[1:]:
-    if not re.fullmatch(towels_regexp(optimized_towels), t):
-        optimized_towels.append(t)
-
-regexp = towels_regexp(optimized_towels)
-
-c = 0
-for d in designs:
-    if re.fullmatch(regexp, d):
-        c += 1
-
-print(c)
-
-towels.sort()
 
 @functools.cache
 def count_ways(design, start_idx=0):
@@ -41,7 +18,12 @@ def count_ways(design, start_idx=0):
             c += count_ways(design, start_idx + len(t))
     return c
 
+c = 0
 s = 0
 for d in designs:
-    s += count_ways(d)
+    w = count_ways(d)
+    s += w
+    if w > 0:
+        c += 1
+print(c)
 print(s)
